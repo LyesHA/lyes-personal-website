9013e80a516760a0ec10c5085cb72ebad5473353
Lyes HADJ AISSA <lyesha526@gmail.com>
2018-11-10 23:44:50 -0500

    website-improved

--- js/script.js
+++ js/script.js
@@ -1,34 +1,47 @@
 $("#btnEduc").click(function() {
     
     $([document.documentElement, document.body]).animate({
-        scrollTop: $("#divEduc").offset().top
+        scrollTop: $("#divEduc").offset().top - 70
     }, 2000);
 });
 
 $("#btnExp").click(function() {
     
     $([document.documentElement, document.body]).animate({
-        scrollTop: $("#divExp").offset().top
+        scrollTop: $("#divExp").offset().top - 70
     }, 2000);
 });
 
 $("#btnSkills").click(function() {
     
     $([document.documentElement, document.body]).animate({
-        scrollTop: $("#divSkills").offset().top
+        scrollTop: $("#divSkills").offset().top - 70
     }, 2000);
 });
 
 $("#btnExtra").click(function() {
     
     $([document.documentElement, document.body]).animate({
-        scrollTop: $("#divExt").offset().top
+        scrollTop: $("#divExt").offset().top - 30
     }, 2000);
 });
 
 $("#btnResume").click(function() {
     
     $([document.documentElement, document.body]).animate({
-        scrollTop: $("#divResume").offset().top
+        scrollTop: $("#divResume").offset().top 
     }, 2000);
+});
+$(document).ready(function(){
+	$(window).on("scroll",function(){
+  	var wn = $(window).scrollTop();
+    if(wn > 300){
+    	$(".navbar").css("background","rgba(3, 53, 99, 1)");
+    }
+    else{
+        $(".navbar").css("background","transparent");
+        $(".navbar").css("transition","500ms ease");
+
+    }
+  });
 });
